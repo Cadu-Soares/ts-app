acc4228953d8ce0d7463221e946de452021b69d7
Cadu-Soares <devcadu.soares@outlook.com>
2022-07-26 12:56:21 -0300

    feat: controle de usuarios

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+
+// class UserController {
+//   findAll(request: Request, response: Response) {
+//     return response.send('Olá, Cadu Soares!')
+//   }
+// }
+
+const UserController = (request: Request, response: Response) => 
+  response.json({
+    name: "User Name",
+    email: "user@user.com",
+  });
+
+export default UserController; 
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express';
+import UserController from './controllers/UserController';
 
 const app = express();
 
-app.get('/', (req, res) =>{
-  res.send('Olá, Cadu!')
-});
+// app.get('/', (req, res) =>{
+
+//   res.send('Olá, Cadu!')
+// });
+
+app.get('/', UserController)
 
 app.listen(3001, () => console.log('Server is listening'));
